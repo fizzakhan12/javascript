bad297e57b41fe36010b071a2a29c5241ce1f7ba
fizzakhan12 <fizzy852@gmail.com>
2020-06-07 07:06:23 -0700

    first commit

---
+++ ch-1/app.js
@@ -0,0 +1,8 @@
+
+alert("Thankyou for visiting this website");
+alert("Error! Please enter a valid password")
+alert("Welcome to Js land...\n Happy Coding!")
+alert("Welcome to Js land")
+alert("Happy Coding \n prevent this page from additional dialogue")
+
+console.log("hello..i can run js through my web browser's console")
